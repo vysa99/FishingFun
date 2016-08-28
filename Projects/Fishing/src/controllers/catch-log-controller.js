18afe0529d119463d9584c2653af392e995fb71a
Vytautas Sakalas <Vytautas Sakalas>
2016-08-28 11:34:05 +0300

    css update

--- Projects/Fishing/src/controllers/catch-log-controller.js
+++ Projects/Fishing/src/controllers/catch-log-controller.js
@@ -10,17 +10,6 @@ angular.module('FishingFun')
 	controller.getFishingLocationById = myDataService.getFishingLocationById;
 	controller.getCatchLog = myDataService.getCatchLog;
 
-	controller.biggestFishSetup = function(biggestFishNo) {
-		// store trophy catch location for view formation
-		myDataService.biggestFish = $filter('arrayPropertyFinder')(myDataService.fishy, 'fishId', myDataService.catchLog[biggestFishNo].catchFishId);
-		myDataService.biggestFishCatch = myDataService.catchLog[biggestFishNo];
-
-		// enable trophy desc only if biggest fish weight >= 1kg
-		if (myDataService.biggestFishCatch.catchWeight >= 1000) {
-			myDataService.trophyVisible = true;
-		}
-	};
-
 	controller.addMissingProperties = function(catchLogEntry) {
 		var newCatchFish = $filter('arrayPropertyFinder')(myDataService.fishy, 'fishId', catchLogEntry.catchFishId);
 
@@ -32,6 +21,36 @@ angular.module('FishingFun')
 		return catchLogEntry;
 	};
 
+	controller.findBiggestFish = function() {
+		var maxWeight = 0;
+		var biggestFishNo = -1;
+
+		for (var n = 0; n < myDataService.catchLog.length ; n++) {
+		// add missing fish properties to catchLog array entries - fish name, image file name, comment text				
+			myDataService.catchLog[n] = controller.addMissingProperties(myDataService.catchLog[n]);
+
+		// check for the biggest catch and get it's index
+			if (myDataService.catchLog[n].catchWeigth > maxWeight) {
+				maxWeight = myDataService.catchLog[n].catchWeigth;
+				biggestFishNo = n;
+			}
+		}
+		return biggestFishNo;
+	}
+
+	controller.biggestFishSetup = function(biggestFishNo) {
+		var biggestFishNo = controller.findBiggestFish();
+
+		// store trophy catch location for view formation
+		myDataService.biggestFish = $filter('arrayPropertyFinder')(myDataService.fishy, 'fishId', myDataService.catchLog[biggestFishNo].catchFishId);
+		myDataService.biggestFishCatch = myDataService.catchLog[biggestFishNo];
+
+		// enable trophy desc only if biggest fish weight >= 1kg
+		if (myDataService.biggestFishCatch.catchWeight >= 1000) {
+			myDataService.trophyVisible = true;
+		}
+	};
+
 	$q.all([requestFactory.catchLogRequest, requestFactory.fishRequest])
 		.then(function(readLocations) {
 			myDataService.catchLog = readLocations[0].data.sort(utilityService.arraySorter('catchDate', false));
@@ -39,19 +58,6 @@ angular.module('FishingFun')
 
 			myDataService.fishy = readLocations[1].data;
 
-			var maxWeight = 0;
-			var biggestFishNo = -1;
-
-			for (var n = 0; n < myDataService.catchLog.length ; n++) {
-			// add missing fish properties to catchLog array entries - fish name, image file name, comment text				
-				myDataService.catchLog[n] = controller.addMissingProperties(myDataService.catchLog[n]);
-
-			// check for the biggest catch and get it's index
-				if (myDataService.catchLog[n].catchWeigth > maxWeight) {
-					maxWeight = myDataService.catchLog[n].catchWeigth;
-					biggestFishNo = n;
-				}
-			}
-			controller.biggestFishSetup(biggestFishNo);
+			controller.biggestFishSetup();
 		});
 }]);
